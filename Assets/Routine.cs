251be9c24e4247aa69306bc1ef76b15ad99105bf
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-16 21:22:58 +1100

    Basic setup for RoutineScript

---
+++ Assets/Routine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class Routine : MonoBehaviour
+{
+
+    GameObject timeManager;
+    public RoutineObject[] routine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeManager = GameObject.FindGameObjectWithTag("TimeManager");
+        TimeManager timeManagerScript = (TimeManager)timeManager.GetComponent(typeof(TimeManager));
+        Debug.Log(timeManagerScript.GetCurrentUITime());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/RoutineObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoutineObject
+{
+    [Range(0, 23)]
+    public int at;
+    public GameObject interestPoint;
+
+    public int GetAt() { return at; }
+    public GameObject GetInterestPoint() { return interestPoint; }
+}
