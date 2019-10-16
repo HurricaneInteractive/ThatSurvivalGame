2ff6c6135e924ea51471ec6a9dfd400bde6ebaf2
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-16 20:52:47 +1100

    TimeManager with 24 hour cycle counter

---
+++ Assets/TimeManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    private readonly int maxTime = 23;
+    private int curTime;
+
+    public int startingTime = 20;
+
+    // Number of seconds a hour lasts
+    public float intervalTime = 5.0f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        curTime = startingTime;
+        InvokeRepeating("IncrementHour", intervalTime, intervalTime);
+    }
+
+    void IncrementHour()
+    {
+        if (curTime == maxTime)
+        {
+            curTime = 0;
+        }
+        else
+        {
+            curTime = curTime += 1;
+        }
+
+        Debug.Log(GetTimeAsString(curTime));
+    }
+
+    public string GetCurrentUITime()
+    {
+        return GetTimeAsString(curTime);
+    }
+
+    public int GetCurrentTime()
+    {
+        return curTime;
+    }
+
+    string GetTimeAsString(int currentTime)
+    {
+        string prefix = "";
+        string suffix = ":00";
+
+        if (currentTime < 10)
+        {
+            prefix = "0";
+        }
+
+        return prefix + currentTime.ToString() + suffix;
+    }
+}
