31c7c8b7860952bc320613beeaa6e499f8036654
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-16 21:39:45 +1100

    Check the current time and determine if villager should move

--- Assets/Routine.cs
+++ Assets/Routine.cs
@@ -7,19 +7,42 @@ public class Routine : MonoBehaviour
 {
 
     GameObject timeManager;
+    TimeManager timeManagerScript;
+    private int oldTime;
+
     public RoutineObject[] routine;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         timeManager = GameObject.FindGameObjectWithTag("TimeManager");
-        TimeManager timeManagerScript = (TimeManager)timeManager.GetComponent(typeof(TimeManager));
-        Debug.Log(timeManagerScript.GetCurrentUITime());
+        timeManagerScript = (TimeManager)timeManager.GetComponent(typeof(TimeManager));
+        oldTime = timeManagerScript.GetCurrentTime();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int currentTime = timeManagerScript.GetCurrentTime();
+        if (routine.Length > 0 && oldTime != currentTime)
+        {
+            for (int i = 0; i < routine.Length; i++)
+            {
+                RoutineObject currentRoutine = routine[i];
+                if (currentRoutine.GetAt() == timeManagerScript.GetCurrentTime())
+                {
+                    MoveToInterestPoint(currentRoutine.GetInterestPoint());
+                }
+            }
 
+            oldTime = currentTime;
+        }
+    }
+
+    void MoveToInterestPoint(GameObject interestPoint)
+    {
+        //Vector3 position = interestPoint.transform.position;
+        Debug.Log("Move to" + interestPoint);
     }
 }
