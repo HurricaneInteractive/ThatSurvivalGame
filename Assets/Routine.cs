84cd9c2cf1816a8225176788a0ff7ba904c043db
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-17 21:40:38 +1100

    Move to interest point at given time

--- Assets/Routine.cs
+++ Assets/Routine.cs
@@ -2,47 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(GameObject))]
 public class Routine : MonoBehaviour
 {
 
-    GameObject timeManager;
-    TimeManager timeManagerScript;
+    private TimeManager timeManager;
     private int oldTime;
+    private GameObject interestPoint;
 
     public RoutineObject[] routine;
-    public GameObject player;
+    public float walkSpeed = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeManager = GameObject.FindGameObjectWithTag("TimeManager");
-        timeManagerScript = (TimeManager)timeManager.GetComponent(typeof(TimeManager));
-        oldTime = timeManagerScript.GetCurrentTime();
+        timeManager = new TimeManagerScript().script;
+        oldTime = timeManager.GetCurrentTime();
     }
 
     // Update is called once per frame
     void Update()
     {
-        int currentTime = timeManagerScript.GetCurrentTime();
+        int currentTime = timeManager.GetCurrentTime();
+
         if (routine.Length > 0 && oldTime != currentTime)
         {
             for (int i = 0; i < routine.Length; i++)
             {
                 RoutineObject currentRoutine = routine[i];
-                if (currentRoutine.GetAt() == timeManagerScript.GetCurrentTime())
+                if (currentRoutine.GetAt() == timeManager.GetCurrentTime())
                 {
-                    MoveToInterestPoint(currentRoutine.GetInterestPoint());
+                    interestPoint = currentRoutine.GetInterestPoint();
                 }
             }
 
             oldTime = currentTime;
         }
+
+        MoveToInterestPoint();
     }
 
-    void MoveToInterestPoint(GameObject interestPoint)
+    void MoveToInterestPoint()
     {
-        //Vector3 position = interestPoint.transform.position;
-        Debug.Log("Move to" + interestPoint);
+        if (interestPoint)
+        {
+            /**
+             * @see https://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+             */
+            float step = walkSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, interestPoint.transform.position, step);
+
+            if (Vector3.Distance(transform.position, interestPoint.transform.position) < 0.001f)
+            {
+                interestPoint = null;
+            }
+        }
     }
 }
