1313f0b5b0bd02d85659aef9f75c052c5594ec8c
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-17 21:41:01 +1100

    TimeManagerScript abstraction

---
+++ Assets/TimeManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManagerScript
+{
+    private GameObject timeManager;
+    public TimeManager script;
+
+    public TimeManagerScript() {
+        timeManager = GameObject.FindGameObjectWithTag("TimeManager");
+        script = (TimeManager)timeManager.GetComponent(typeof(TimeManager));
+    }
+}
