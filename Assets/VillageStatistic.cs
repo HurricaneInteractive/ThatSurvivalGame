1b210ebf00c92776908f916aa56c0a5b2687a444
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-17 21:40:07 +1100

    VillageStatistic base class

---
+++ Assets/VillageStatistic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageStatistic
+{
+    private TimeManager timeManager = new TimeManagerScript().script;
+    private float dayLength;
+    private float decreasePerSecond;
+
+    public float decreaseRate;
+    public float currentValue;
+
+    public VillageStatistic(float startingValue, float rate)
+    {
+        currentValue = startingValue;
+        decreaseRate = rate;
+
+        dayLength = timeManager.intervalTime;
+        decreasePerSecond = rate / dayLength;
+    }
+
+    public void DecreaseStatistic()
+    {
+        currentValue = (float)Math.Round(currentValue - decreasePerSecond, 2);
+    }
+
+    public void UpdateDecreaseRateBy(float value)
+    {
+        decreaseRate += value;
+        decreasePerSecond = decreaseRate / dayLength;
+    }
+}
