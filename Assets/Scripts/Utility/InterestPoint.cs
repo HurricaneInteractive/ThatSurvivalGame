965d7666a64d39346e16ffdf72e67f34f4dda135
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-19 15:10:19 +1100

    Visually hide the interest points

---
+++ Assets/Scripts/Utility/InterestPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class InterestPoint : MonoBehaviour
+{
+    private Renderer render;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = gameObject.GetComponent<Renderer>();
+        render.enabled = false;
+    }
+}
