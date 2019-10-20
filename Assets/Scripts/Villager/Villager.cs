7a5e352e845465ab6761eb5ed3f0d1b232f03bfb
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-20 21:10:28 +1100

    Added Villager script to handle shader swapping

---
+++ Assets/Scripts/Villager/Villager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villager : MonoBehaviour
+{
+    public GameObject villager;
+    private Renderer villagerRenderer;
+    private Shader outlineShader;
+    private Shader defaultShader;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        villagerRenderer = villager.GetComponent<Renderer>();
+        outlineShader = Shader.Find("Custom/OutlineShader");
+        defaultShader = villagerRenderer.material.shader;
+    }
+
+    private void OnMouseOver() {
+        setCustomOutlineShader();
+    }
+
+    private void OnMouseExit() {
+        resetToDefaultShader();
+    }
+
+    public void setCustomOutlineShader() {
+        villagerRenderer.material.shader = outlineShader;
+    }
+
+    public void resetToDefaultShader() {
+        villagerRenderer.material.shader = defaultShader;
+    }
+}
