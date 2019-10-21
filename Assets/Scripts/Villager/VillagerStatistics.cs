6b9186a701dd4ac552af02528ecdecf7e6f689d7
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-21 21:21:27 +1100

    Generate villager scavenge stat and assign a random name

--- Assets/Scripts/Villager/VillagerStatistics.cs
+++ Assets/Scripts/Villager/VillagerStatistics.cs
@@ -1,5 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+struct JSONData {
+    public List<string> names;
+}
 
 public class VillagerStatistics : MonoBehaviour {
+    public new string name;
 
+    public int hunger = 0;
+    public int thirst = 0;
+    public float morale = 100.0f;
+
+    private int scavengingAbilityMin = 0;
+    private int scavengingAbilityMax = 10;
+    public int scavengingAbility = 0;
+
+    private void Start() {
+        scavengingAbility = (int)Mathf.Round(Random.Range(scavengingAbilityMin, scavengingAbilityMax));
+        List<string> names = GetNameList();
+        if (names.Count > 0) {
+            name = names[(int)Mathf.Round(Random.Range(0, names.Count))];
+        }
+    }
+
+    public List<string> GetNameList() {
+        string filePath = Path.Combine(Application.streamingAssetsPath, "names.json");
+        if (File.Exists(filePath)) {
+            string dataAsJson = File.ReadAllText(filePath);
+            JSONData data = JsonUtility.FromJson<JSONData>(dataAsJson);
+            return data.names;
+        }
+        else {
+            Debug.Log("File not found");
+        }
+
+        return new List<string>();
+    }
 }
