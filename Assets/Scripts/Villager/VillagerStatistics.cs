921d2e4206110c1444e1ba18af471d8769c08464
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-10-22 20:21:24 +1100

    Added hunger and thirst statistics to Villager

--- Assets/Scripts/Villager/VillagerStatistics.cs
+++ Assets/Scripts/Villager/VillagerStatistics.cs
@@ -8,19 +8,34 @@ struct JSONData {
 }
 
 public class VillagerStatistics : MonoBehaviour {
-    public new string name;
+    public new string name = "404";
+    private TimeManager timeManager;
 
-    public int hunger = 0;
-    public int thirst = 0;
-    public float morale = 100.0f;
+    VillageStatistic hunger;
+    public float hungerValue = 5.0f;
+    public float hungerDecreaseRate = 1.0f;
+
+    VillageStatistic thirst;
+    public float thirstValue = 3.0f;
+    public float thirstDecreaseRate = 1.0f;
 
     private int scavengingAbilityMin = 0;
     private int scavengingAbilityMax = 10;
     public int scavengingAbility = 0;
 
     private void Start() {
+        timeManager = new TimeManagerScript().script;
+        float dayLength = timeManager.GetDayLength();
+
+        hunger = new VillageStatistic(hungerValue, hungerDecreaseRate);
+        thirst = new VillageStatistic(thirstValue, thirstDecreaseRate);
+
+        InvokeRepeating("DecreaseHunger", dayLength, dayLength);
+        InvokeRepeating("DecreaseThirst", dayLength, dayLength);
+
         scavengingAbility = (int)Mathf.Round(Random.Range(scavengingAbilityMin, scavengingAbilityMax));
         List<string> names = GetNameList();
+
         if (names.Count > 0) {
             name = names[(int)Mathf.Round(Random.Range(0, names.Count))];
         }
@@ -33,10 +48,17 @@ public class VillagerStatistics : MonoBehaviour {
             JSONData data = JsonUtility.FromJson<JSONData>(dataAsJson);
             return data.names;
         }
-        else {
-            Debug.Log("File not found");
-        }
 
         return new List<string>();
+    }
+
+    public void DecreaseHunger() {
+        hunger.DecreaseStatistic();
+        hungerValue = hunger.currentValue;
+    }
+
+    public void DecreaseThirst() {
+        thirst.DecreaseStatistic();
+        thirstValue = thirst.currentValue;
     }
 }
